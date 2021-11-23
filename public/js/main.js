f7558f5edeacf0e11d2920c97bd0426e9fc357fc
Sean O'Brien <60306702+stobrien89@users.noreply.github.com>
2021-11-22 20:55:50 -0800

    add tileset

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,17 @@
+function loadImage(url) {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image);
+        })
+    })
+    image.src = url;
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-context.fillRect(0, 0, 50, 50)
+loadImage('/img/tileset.png')
+.then(img => {
+    context.drawImage(img, 0 , 0);
+})
